8cb0c1d02bcfee586540b9c3453d2e96de9503b9
EULIR <runmingli_edu@163.com>
2018-01-20 12:53:02 +0800

    [ init ]

--- src/Project64.java
+++ src/Project64.java
@@ -1,18 +1,32 @@
 public class Project64
 {
+	private static boolean isSquare(int a)
+	{
+		int b = (int) Math.sqrt(a);
+		return b * b == a;
+	}
+
 	public static void main(String[] args)
 	{
+		int ans = 0;
 		for (int i = 1; i <= 10000; i++)
 		{
+			int period = 0;
 			int m = 0;
 			int d = 1;
 			int a = (int) Math.floor(Math.sqrt(i));
-			while (a != 0)
+			if (isSquare(i))
+				continue;
+			while (a != 2 * (int) Math.floor(Math.sqrt(i)))
 			{
 				m = d * a - m;
-				d = (int) ((i - m * m) / d);
-				a = (int) Math.floor(Math.floor(i));
+				d = (i - m * m) / d;
+				a = (int) Math.floor(((int) Math.floor(Math.sqrt(i)) + m)/d);
+				period++;
 			}
+			if (period % 2 == 1)
+				ans++;
 		}
+		System.out.println(ans);
 	}
 }
