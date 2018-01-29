80f97091f2776220cdbe9d32f7f9938e3de87021
EULIR <runmingli_edu@163.com>
2018-01-29 23:41:50 +0800

    [ init ] initial commit

---
+++ src/Project071.java
@@ -0,0 +1,59 @@
+import java.util.Iterator;
+import java.util.TreeMap;
+
+class Fraction
+{
+	private int a;
+	private int b;
+
+	public Fraction(int n, int r)
+	{
+		a = n;
+		b = r;
+	}
+
+	public int getB()
+	{
+		return b;
+	}
+
+	public int getA()
+	{
+		return a;
+	}
+}
+
+public class Project071
+{
+	public static void main(String[] args)
+	{
+		TreeMap<Double, Fraction> map = new TreeMap<>();
+		for (int i = 2; i <= 1000000; i++)
+		{
+			int j = (int) Math.floor(((double) 3 / 7) * i);
+			double d = (double) j / i;
+			if (d > 0.428570 && d < 0.428572)
+				if (Library.gcd(i, j) == 1)
+				{
+					Fraction fra = new Fraction(j, i);
+					map.put(d, fra);
+				}
+		}
+		Iterator<Double> it = map.keySet().iterator();
+		int lastNumerator = 0;
+		//int lastDenominator = 0;
+		while (it.hasNext())
+		{
+			double a = it.next();
+			Fraction b = map.get(a);
+			if (b.getA() == 3 && b.getB() == 7)
+			{
+				System.out.println(lastNumerator);
+				System.exit(0);
+			}
+			lastNumerator = b.getA();
+			//lastDenominator = b.getB();
+			//System.out.println(a + " " + b);
+		}
+	}
+}
