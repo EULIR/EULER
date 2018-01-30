8c6d4ee76823330b9ff16199ece98623c9436339
EULIR <runmingli_edu@163.com>
2018-01-30 17:22:34 +0800

    [ init ] initial commit

---
+++ src/Project072.java
@@ -0,0 +1,25 @@
+public class Project072
+{
+	private static long euler(long n)
+	{
+		long res = n, a = n;
+		for (long i = 2; i * i <= a; i++)
+			if (a % i == 0)
+			{
+				res = res / i * (i - 1);
+				while (a % i == 0)
+					a /= i;
+			}
+		if (a > 1)
+			res = res / a * (a - 1);
+		return res;
+	}
+
+	public static void main(String[] args)
+	{
+		long ans = 0;
+		for (int i = 2; i <= 1000000; i++)
+			ans += euler(i);
+		System.out.println(ans);
+	}
+}
