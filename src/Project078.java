872b6cf31132debd81438184a539388e207719e0
EULIR <runmingli_edu@163.com>
2018-02-04 14:25:37 +0800

    [ init ] initial commit

---
+++ src/Project078.java
@@ -0,0 +1,27 @@
+public class Project078
+{
+	private static void cal(int a)
+	{
+		int[] arr = new int[a];
+		arr[0] = 1;
+		for (int i = 1; i < a; i++)
+			for (int j = i; j < a; j++)
+			{
+				arr[j] += arr[j - i];
+				arr[j] %= 1000000;
+			}
+		for (int i = 0; i < a; i++)
+			if (arr[i] == 0)
+			{
+				System.out.println(i);
+				System.exit(0);
+			}
+	}
+
+	public static void main(String[] args)
+	{
+		int a = 1;
+		while (true)
+			cal(a *= 2);
+	}
+}
