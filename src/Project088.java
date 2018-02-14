75df5f3d8b873bbe0bca3d7a2fbe22cd75da02d7
EULIR <runmingli_edu@163.com>
2018-02-14 22:11:00 +0800

    [ init ] initial commit

---
+++ src/Project088.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Project088
+{
+	private static boolean judge(int p, int s, int k)
+	{
+		if (s < k)
+			return false;
+		if (p == 1)
+			return s == k;
+		if (k == 1)
+			return p == s;
+		for (int i = 2; i <= p && i <= s - k + 1; i++)
+			if (p % i == 0)
+				if (judge(p / i, s - i, k - 1))
+					return true;
+		return false;
+	}
+
+	public static void main(String[] args)
+	{
+		int sum = 0;
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 2; i <= 12000; i++)
+		{
+			int n = i + 1;
+			while (true)
+			{
+				if (judge(n, n, i))
+				{
+					if (!list.contains(n))
+					{
+						list.add(n);
+						sum += n;
+					}
+					break;
+				}
+				n++;
+			}
+		}
+		System.out.println(sum);
+	}
+}
