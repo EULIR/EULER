3178b907def5f21a486601c858de6e23cd6e8c32
EULIR <runmingli_edu@163.com>
2018-02-17 21:46:55 +0800

    update the method

--- src/Project091.java
+++ src/Project091.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Project091
 {
-	private static boolean isRight(int x1, int y1, int x2, int y2)
+	private static boolean isRightTriangle(int x1, int y1, int x2, int y2)
 	{
 		double distanceOne = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 		double distanceTwo = Math.sqrt(x1 * x1 + y1 * y1);
@@ -19,7 +19,7 @@ public class Project091
 			for (int x2 = 0; x2 <= 50; x2++)
 				for (int y1 = 0; y1 <= 50; y1++)
 					for (int y2 = 0; y2 <= 50; y2++)
-						if (!(x1 == x2 && y1 == y2) && !(x1 == 0 && y1 == 0) && !(x2 == 0 && y2 == 0) && isRight(x1, y1, x2, y2))
+						if (!(x1 == x2 && y1 == y2) && !(x1 == 0 && y1 == 0) && !(x2 == 0 && y2 == 0) && isRightTriangle(x1, y1, x2, y2))
 							ans++;
 		System.out.println(ans / 2);
 	}
