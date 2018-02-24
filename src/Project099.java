7c799df29086189133d20ca4c0922a9b04d7dfd5
EULIR <runmingli_edu@163.com>
2018-02-24 21:47:04 +0800

    [ init ] initial commit

---
+++ src/Project099.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Project099
+{
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		for (int i = 0; i < 1000; i++)
+		{
+			String str = s.nextLine();
+			int base = Integer.parseInt(str.substring(0,str.indexOf(",")));
+			int exp = Integer.parseInt(str.substring(str.indexOf(",") + 1));
+			System.out.println(base);
+			System.out.println(exp);
+		}
+	}
+}
