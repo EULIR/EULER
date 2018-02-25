db227bf2509736cd60f06e9cbdfe8e578dc67e90
EULIR <runmingli_edu@163.com>
2018-02-25 20:08:33 +0800

    [ init ] initial commit

--- src/Project099.java
+++ src/Project099.java
@@ -5,8 +5,7 @@ public class Project099
 	public static void main(String[] args)
 	{
 		Scanner s = new Scanner(System.in);
-		for (int i = 0; i < 1000; i++)
-		{
+		for (int i = 0; i < 1000; i++) {
 			String str = s.nextLine();
 			int base = Integer.parseInt(str.substring(0,str.indexOf(",")));
 			int exp = Integer.parseInt(str.substring(str.indexOf(",") + 1));
