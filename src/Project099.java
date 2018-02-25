47a4cab6f017354c775825b44ee84a29462dba4b
EULIR <runmingli_edu@163.com>
2018-02-25 20:51:10 +0800

    [ init ] initial commit

--- src/Project099.java
+++ src/Project099.java
@@ -5,13 +5,20 @@ public class Project099
 	public static void main(String[] args)
 	{
 		Scanner s = new Scanner(System.in);
+		double max = 0.00;
+		int maxIndex = 0;
 		for (int i = 0; i < 1000; i++)
 		{
 			String str = s.nextLine();
 			int base = Integer.parseInt(str.substring(0,str.indexOf(",")));
 			int exp = Integer.parseInt(str.substring(str.indexOf(",") + 1));
-			System.out.println(base);
-			System.out.println(exp);
+			double result = exp * Library.log(10, base);
+			if (result > max)
+			{
+				max = result;
+				maxIndex = i;
+			}
 		}
+		System.out.println(++maxIndex);
 	}
 }
