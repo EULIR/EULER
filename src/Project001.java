c2d47a7f545b53347949b60fe7dfb31c3c51fa12
EULIR <runmingli_edu@163.com>
2018-03-04 16:26:38 +0800

    update problem1

--- src/Project001.java
+++ src/Project001.java
@@ -2,16 +2,9 @@ public class Project001
 {
 	public static void main(String... args)
 	{
-		boolean[] booleans = new boolean[1000];
-		for (int i = 1; i < 1000; i++)
-			booleans[i] = false;
 		int ans = 0;
-		for (int i = 0; i < 1000; i += 3)
-			booleans[i] = true;
-		for (int j = 0; j < 1000; j += 5)
-			booleans[j] = true;
 		for (int i = 1; i < 1000; i++)
-			if (booleans[i])
+			if (i % 3 == 0 || i % 5 == 0)
 				ans += i;
 		System.out.println(ans);
 	}
