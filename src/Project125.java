612f6c26dccf09089d1e57284ab6e1508a950b5f
EULIR <runmingli_edu@163.com>
2018-12-25 13:27:14 +0800

    update project125

---
+++ src/Project125.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Project125 {
+	public static void main(String[] args) {
+		double max = Math.sqrt(100000000);
+		long sum = 0;
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 1; i <= max; i++) {
+			int number = i * i;
+			for (int j = i + 1; j <= max; j++) {
+				number += j * j;
+				if (number > 100000000)
+					break;
+				if (Library.isReverse(number) && !list.contains(number)) {
+					sum += number;
+					list.add(number);
+				}
+			}
+		}
+		System.out.println(sum);
+	}
+}
