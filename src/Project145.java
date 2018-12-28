50f6854c83dfaab8f3cd224cc2f61e9553d132a7
EULIR <runmingli_edu@163.com>
2018-12-28 20:52:08 +0800

    update project145

--- src/Project145.java
+++ src/Project145.java
@@ -12,8 +12,13 @@ public class Project145 {
 	public static void main(String[] args) {
 		long num = 0;
 		for (long i = 1; i < 1000000000L; i++) {
-			System.out.println(i);
+			//System.out.println(i);
 			long b = Long.parseLong(Library.reverse(String.valueOf(i)));
+			//this algorithm is totally stupid! I found myself stupid immediately after I wrote this code
+			//But I kept it to remind myself every time I see it
+			//if (i % 10 == 0)
+			if (String.valueOf(b).length() != String.valueOf(i).length())
+				continue;
 			if (isReversible(i + b))
 				num++;
 		}
