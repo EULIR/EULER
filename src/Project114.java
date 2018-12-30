99971a2d3c1f29bc9f2b9b52e033afb758dc0fca
EULIR <runmingli_edu@163.com>
2018-12-30 23:13:04 +0800

    update project114

---
+++ src/Project114.java
@@ -0,0 +1,24 @@
+import java.math.BigInteger;
+
+public class Project114 {
+	public static BigInteger Combination(BigInteger m, BigInteger n) {
+		BigInteger ans = BigInteger.ONE;
+		BigInteger b = BigInteger.ONE;
+		if (m.compareTo(n.subtract(m)) > 0)
+			m = n.subtract(m);
+		for (BigInteger i = BigInteger.ONE; i.compareTo(m) <= 0; i = i.add(BigInteger.valueOf(1))) {
+			ans = ans.multiply(n.subtract(m).add(i));
+			b = b.multiply(i);
+			if (ans.mod(b).compareTo(BigInteger.ZERO) == 0) {
+				ans = ans.divide(b);
+				b = BigInteger.ONE;
+			}
+
+		}
+		return ans.divide(b);
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
