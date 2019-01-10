b102894fa4a18a04856589eb86ce20e90af2db29
EULIR <runmingli_edu@163.com>
2019-01-10 09:44:56 +0800

    update project124

--- src/Project124.java
+++ src/Project124.java
@@ -1,31 +1,55 @@
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Project124 {
-	public static boolean isPower(int power, int base) {
-		boolean result;
-		if (power <= 0 || !(power % base == 0))
-			return false;
-		if (power / base > 1)
-			result = isPower(power / base, base);
-		else result = power % base == 0;
-		return result;
-	}
-
-	public static boolean isLegal(ArrayList<Integer> prev, int a) {
-		return true;
+	public static int factorProduct(int n) {
+		HashSet<Integer> set = new HashSet<>();
+		for (int j = 2; j <= Math.sqrt(n); j++) {
+			if (n % j == 0) {
+				set.add(j);
+				n /= j;
+				j = 1;
+			}
+		}
+		if (n > 1)
+			set.add(n);
+		int prod = 1;
+		for (Integer aSet : set) {
+			//System.out.println(aSet);
+			prod *= aSet;
+		}
+		return prod;
 	}
 
 	public static void main(String[] args) {
-		ArrayList<Integer> prev = new ArrayList<>();
-		int i = 1;
-		int num = 1;
-		while (num < 10000) {
+		int[][] arr = new int[100001][2];
+		arr[1][0] = arr[1][1] = 1;
+		for (int i = 2; i < arr.length; i++) {
+			arr[i][0] = factorProduct(i);
+			arr[i][1] = i;
+		}
+		cocktailSort(arr);
+//		for (int i = 0; i < arr.length; i++) {
+//			System.out.println(Arrays.toString(arr[i]));
+//		}
+		System.out.println(arr[10000][1]);
+	}
 
-			if (isLegal(prev, i)) {
-
+	public static void cocktailSort(int[][] arr) {
+		for (int i = 0; i < arr.length / 2; i++) {
+			for (int j = i; j < arr.length - i - 1; j++) {
+				if (arr[j][0] > arr[j + 1][0]) {
+					int[] temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+			for (int j = arr.length - 1 - (i + 1); j > i; j--) {
+				if (arr[j][0] < arr[j - 1][0]) {
+					int[] temp = arr[j];
+					arr[j] = arr[j - 1];
+					arr[j - 1] = temp;
+				}
 			}
 		}
-		System.out.println(num);
-		System.out.println(i);
 	}
 }
