363129dcea979c2bb7d55e54c19d8865a17bccdb
EULIR <runmingli_edu@163.com>
2019-01-18 23:05:51 +0800

    update project121

--- src/Project121.java
+++ src/Project121.java
@@ -1,4 +1,4 @@
-import java.util.stream.IntStream;
+import java.math.BigInteger;
 
 public class Project121 {
 	public static long[] polyMultiply(long[] a, long[] b) {
@@ -12,21 +12,26 @@ public class Project121 {
 	}
 
 	/*
-	* (x+1)(x+2)(x+3)...(x+15)=
-	* a1*x^15 + a2*x^14 + a3*x^13 + ... + a15*x^1 + a16*x^0
-	* a1+a2+a3+...+a8 = sum
-	* 16!=total
-	* */
+	 * (x+1)(x+2)(x+3)...(x+15)=
+	 * a1*x^15 + a2*x^14 + a3*x^13 + ... + a15*x^1 + a16*x^0
+	 * a1+a2+a3+...+a8 = sum
+	 * 16!=total
+	 * */
 	public static void main(String[] args) {
+		int n = 29;
 		long[] formula = {1L};
-		for (int i = 1; i <= 15; i++) {
+		for (int i = 1; i <= n; i++) {
 			formula = polyMultiply(formula, new long[]{i, 1});
 		}
 		long sum = 0;
-		for (int i = 8; i < formula.length; i++) {
+		for (int i = n / 2 + 1; i < formula.length; i++) {
 			sum += formula[i];
 		}
-		long total = IntStream.rangeClosed(1, 16).asLongStream().reduce(1, (a, b) -> a * b);
-		System.out.println(total / sum);
+		BigInteger total = BigInteger.ONE;
+		for (int i = 1; i <= n + 1; i++) {
+			total = total.multiply(BigInteger.valueOf(i));
+		}
+		//long total = IntStream.rangeClosed(1, n + 1).asLongStream().reduce(1, (a, b) -> a * b);
+		System.out.println(total.divide(BigInteger.valueOf(sum)));
 	}
 }
