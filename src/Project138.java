440e374c34687015505ba5e9f3c2f68f7573996c
EULIR <runmingli_edu@163.com>
2019-01-25 23:27:25 +0800

    update project138

--- src/Project138.java
+++ src/Project138.java
@@ -1,25 +1,37 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Project138 {
 	public static void main(String[] args) {
 		int k = 0;
-		long i = 3;
-		long ans = 0;
+		BigDecimal i = new BigDecimal(3);
+		BigDecimal ans = new BigDecimal(0);
 		while (k < 12) {
-			i++;
-			double l1 = Math.sqrt((i + 1) * (i + 1) + ((double) i / 2) * ((double) i / 2));
-			long ll1 = (long) l1;
-			if (Math.abs(ll1 - l1) < 0.000000000000000000000000000000000000000000000000000000000001 && 2 * ll1 > i) {
-				k++;
-				ans += ll1;
-				System.out.println(i + " " + ll1);
-			}
-			double l2 = Math.sqrt((i - 1) * (i - 1) + ((double) i / 2) * ((double) i / 2));
-			long ll2 = (long) l2;
-			if (Math.abs(ll2 - l2) < 0.000000000000000000000000000000000000000000000000000000000001 && 2 * ll2 > i) {
-				k++;
-				ans += ll2;
-				System.out.println(i + " " + ll2);
-			}
+			i = i.add(BigDecimal.valueOf(1));
+			BigDecimal a = i.add(BigDecimal.valueOf(1)).pow(2);
+			BigDecimal b = BigDecimal.valueOf(2).divide(i, 1024, RoundingMode.UNNECESSARY).pow(2);
+			BigDecimal l1 = a.add(b);
+//			long ll1 = (long) l1;
+//			if (Math.abs(ll1 - l1) < 0.000000000000000000000000000000000000000000000000000000000001 && 2 * ll1 > i) {
+//				k++;
+//				ans += ll1;
+//				System.out.println(i + " " + ll1);
+//			}
+//			double l2 = Math.sqrt((i - 1) * (i - 1) + ((double) i / 2) * ((double) i / 2));
+//			long ll2 = (long) l2;
+//			if (Math.abs(ll2 - l2) < 0.000000000000000000000000000000000000000000000000000000000001 && 2 * ll2 > i) {
+//				k++;
+//				ans += ll2;
+//				System.out.println(i + " " + ll2);
+			//}
 		}
-		System.out.println(ans);
+		//System.out.println(ans);
+	}
+
+	public static BigDecimal sqrt(BigDecimal d) {
+		BigDecimal error = new BigDecimal("1e-15");
+		BigDecimal t = new BigDecimal(String.valueOf(d));
+		BigDecimal a;
+		return null;
 	}
 }
