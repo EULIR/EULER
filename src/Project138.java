d7cfe0990ee2e227b1727a87c00984cc4928369b
EULIR <runmingli_edu@163.com>
2019-01-27 23:08:03 +0800

    update project138

--- src/Project138.java
+++ src/Project138.java
@@ -1,37 +1,14 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.text.MessageFormat;
 
 public class Project138 {
 	public static void main(String[] args) {
-		int k = 0;
-		BigDecimal i = new BigDecimal(3);
-		BigDecimal ans = new BigDecimal(0);
-		while (k < 12) {
-			i = i.add(BigDecimal.valueOf(1));
-			BigDecimal a = i.add(BigDecimal.valueOf(1)).pow(2);
-			BigDecimal b = BigDecimal.valueOf(2).divide(i, 1024, RoundingMode.UNNECESSARY).pow(2);
-			BigDecimal l1 = a.add(b);
-//			long ll1 = (long) l1;
-//			if (Math.abs(ll1 - l1) < 0.000000000000000000000000000000000000000000000000000000000001 && 2 * ll1 > i) {
-//				k++;
-//				ans += ll1;
-//				System.out.println(i + " " + ll1);
-//			}
-//			double l2 = Math.sqrt((i - 1) * (i - 1) + ((double) i / 2) * ((double) i / 2));
-//			long ll2 = (long) l2;
-//			if (Math.abs(ll2 - l2) < 0.000000000000000000000000000000000000000000000000000000000001 && 2 * ll2 > i) {
-//				k++;
-//				ans += ll2;
-//				System.out.println(i + " " + ll2);
-			//}
+		double n = 4.0D;
+		double ans = 0;
+		for (int i = 2; i <= 12; i++) {
+			double m = (2 * n) + Math.sqrt((5 * n * n) + (i % 2 == 0 ? 1 : -1));
+			ans += m * m + n * n;
+			n = m;
 		}
-		//System.out.println(ans);
-	}
-
-	public static BigDecimal sqrt(BigDecimal d) {
-		BigDecimal error = new BigDecimal("1e-15");
-		BigDecimal t = new BigDecimal(String.valueOf(d));
-		BigDecimal a;
-		return null;
+		System.out.println(MessageFormat.format("{0}", ans));
 	}
 }
